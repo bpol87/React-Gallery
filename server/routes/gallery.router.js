97ffc365b1a412bac745cf549e3036d05911331c
Ben Pollack <benpollack8723@gmail.com>
2024-08-09 15:55:52 -0500

    working base framework

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pool = require('../modules/pool.js');
 const router = express.Router();
 
 // PUT /gallery/like/:id
@@ -6,9 +7,23 @@ router.put('/like/:id', (req, res) => {
   // code here
 });
 
+
 // GET /gallery
 router.get('/', (req, res) => {
-  // code here
+  const sqlText = `
+      SELECT * FROM "gallery"
+        ORDER BY "id";
+    `
+  
+    pool.query(sqlText)
+      .then((dbRes) => {
+        console.log(`Got stuff from database`, dbRes.rows);
+        res.send(dbRes.rows)
+      })
+      .catch((dbErr) => {
+        console.log('GET /api/gallery-list dbErr:', dbErr)
+        res.sendStatus(500)
+      })
 });
 
 module.exports = router;
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,11 +1,36 @@
 import GalleryItem from "../GalleryItem/GalleryItem";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function GalleryList() {
-    return (
-        <>
-            <GalleryItem />
-        </>
-    )
-}
+    const [fullGalleryList, setFullGalleryList] = useState([]);
 
-export default GalleryList;
+    useEffect(() => {
+        getGallery();
+    }, []);
+
+    const getGallery = () => {
+        axios({
+            method: 'GET',
+            url: '/api/gallery'
+        })
+            .then((response) => {
+                console.log(response.data);
+                setFullGalleryList(response.data);
+            })
+            .catch((err) => {
+                console.log('GET /api/gallery had an error', err)
+                alert('There was an error fetching the Gallery.')
+            })}
+
+        return (
+            <div>
+               {fullGalleryList.map((card) => {
+                console.log(card.url);
+                return <GalleryItem key={card.id} card={card}/>
+               })}
+            </div>
+        )
+    }
+
+    export default GalleryList;
